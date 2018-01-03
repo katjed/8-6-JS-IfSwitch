887ccb1dbb230e838d53c536aac9171f7551fadb
Katarzyna Jedrzejczak <katarzyna.jedrzejczak@op.pl>
2018-01-03 03:29:40 +0100

    Change js/script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+//var name = prompt('Enter your name');
+//alert('Hello, ' + name);
+//console.log('Hello, ' + name);
+
+var a = 2,
+	b = 3,
+	value = (a * a) - (2 * a * b) + (b * b);
+console.log('Value is equal to ' + [(a * a) - (2 * a * b) + (b * b)]);
+
+if(value > 0) {
+	console.log('Value is bigger than 0');
+} else {
+	console.log('Value is less than 0');
+}
+
+var value = 0 ? 'Value is equal to 0' : 'Value is not equal to 0';
+console.log(value);
