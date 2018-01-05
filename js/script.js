09a9bf571535ba655e6f70ef7b18e4b242ac2201
Katarzyna Jedrzejczak <katarzyna.jedrzejczak@op.pl>
2018-01-05 07:51:20 +0100

    Change js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 var a = 2,
 	b = 3,
 	value = (a * a) - (2 * a * b) + (b * b);
-console.log('Value is equal to: ' + [(a * a) - (2 * a * b) + (b * b)]);
+console.log('Value is equal to: ' + value);
 
 if(value > 0) {
-	console.log('Value is bigger than 0');
-} else {
-	console.log('Value is less than 0');
+	console.log('Value is positive');
+} else if(value < 0) {
+	console.log('Value is negative');
 }
 
 var value = 0 ? 'Value is equal to 0' : 'Value is not equal to 0';
