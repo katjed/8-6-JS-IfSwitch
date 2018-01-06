28c5a0350822d1881726fea1acf107ef2b8011b9
Katarzyna Jedrzejczak <katarzyna.jedrzejczak@op.pl>
2018-01-06 02:36:31 +0100

    Change sass, change js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 var a = 2,
 	b = 3,
-	value = (a * a) - (2 * a * b) + (b * b);
+	value = 0;           //(a * a) - (2 * a * b) + (b * b)
 console.log('Value is equal to: ' + value);
 
 if(value > 0) {
 	console.log('Value is positive');
 } else if(value < 0) {
 	console.log('Value is negative');
-}
+} 
 
 var value = 0 ? 'Value is equal to 0' : 'Value is not equal to 0';
-console.log(value);
+console.log(value); 
+
+/* if(value = 0) {
+	console.log('Value is equal to 0');
+} else {
+	console.log('Value is not equal to 0');
+} */
+
