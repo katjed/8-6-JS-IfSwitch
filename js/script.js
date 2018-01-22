65a3a5d783cb67f3c6fd38ff8abea7b07862a2c0
Katarzyna Jedrzejczak <katarzyna.jedrzejczak@op.pl>
2018-01-22 04:00:38 +0100

    Update index.html, update js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
+// method 1 
+
 var a = 2,
 	b = 3,
-	value = 0;           //(a * a) - (2 * a * b) + (b * b)
+	value = (a * a) - (2 * a * b) + (b * b);
 console.log('Value is equal to: ' + value);
 
 if(value > 0) {
@@ -9,12 +11,31 @@ if(value > 0) {
 	console.log('Value is negative');
 } 
 
-var value = 0 ? 'Value is equal to 0' : 'Value is not equal to 0';
-console.log(value); 
-
-/* if(value = 0) {
+if(value == 0) {
 	console.log('Value is equal to 0');
 } else {
 	console.log('Value is not equal to 0');
-} */
+} 
 
+// method 2
+
+var value = value == 0 ? 'Value is equal to 0' : 'Value is not equal to 0';
+console.log(value); 
+
+// method 3
+
+var a = prompt('Enter a'),
+	b = prompt('Enter b'),
+	value = (a * a) - (2 * a * b) + (b * b);
+alert('Value is equal to: ' + value)
+console.log('Value is equal to: ' + value);
+
+if (value > 0) {
+	console.log('Value is positive');
+} else if (value < 0) {
+	console.log('Value is negative');
+} else if (value == 0) {
+	console.log('Value is equal to 0');
+} else {
+	console.log('Wrong data');
+}
